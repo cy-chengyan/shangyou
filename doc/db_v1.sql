ce9d0a27413ff75fcb20dc80b982223d02884aa7
Drew-Cheng <wdrewcheng@163.com>
2019-12-04 11:39:40 +0800

    修改用户表gender字段，收藏夹表status字段的状态值；修改邮票详情表中枚数字段的数据类型；新增小版表，赠送版表，小本票表，小型张表。

--- doc/db_v1.sql
+++ doc/db_v1.sql
@@ -4,8 +4,8 @@ ALTER DATABASE 	shangyou_v1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci
 
 use shangyou_v1;
 
-create user 'qilpsian'@'%'identified by '密码';
-grant all on shangyou_v1.*to'qilpsian'@'%';
+create user 'qilpsian'@'%' identified by '密码';
+grant all on shangyou_v1.* to 'qilpsian'@'%';
 
 --用户表
 DROP TABLE IF EXISTS t_user;
@@ -13,7 +13,7 @@ CREATE TABLE t_user(
        'uid' char(16) PRIMARY KEY COMMENT '用户id',
        'mobile_number' varchar(32) NOT NULL COMMENT '手机号码',
        'nickname' varchar(32）NOT NULL COMMENT ‘昵称’，
-       'gender' tinyint(1) NOT NULL COMMENT '性别,0未知,1男,2女',
+       'gender' tinyint(1) NOT NULL COMMENT '性别,1男,2女,3未知',
        'created_at' bigint(20) NOT NULL COMMENT '创建时间',
        'updated_at' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP()
         ON UPDATE CURRENT_TIMESTAMP() COMMENT '更新时间'
@@ -30,7 +30,7 @@ CREATE TABLE t_favorite(
        'created_at' bigint(20) NOT NULL COMMENT '创建时间',
        'updated_at' timestamp NOT NULL COMMENT CURRENT_TIMESTAMP()
        ON UPDATE CURRENT_TIMESTAMP() COMMENT '更新时间',
-       'status' tinyint(1) NOT NULL COMMENT '0未收藏,1收藏');  
+       'status' tinyint(1) NOT NULL COMMENT '1收藏,2未收藏');  
 
 --邮票详情表
 DROP TABLE IF EXISTS t_stamp;
@@ -38,37 +38,57 @@ CREATE TABLE t_stamp(
        'stid' varchar(16) PRIMARY KEY COMMENT '邮票id',
        'title' varchar(16) NOT NULL COMMENT '邮票标题',
        'picture' varchar(256) NOT COMMENT '邮票图片',
-       'facevalue' varchar(8) NOT NULL COMMENT '邮票面值',
+       'facevalue' varchar(16) NOT NULL COMMENT '邮票面值',
        'number' varchar(16) NOT NULL COMMENT '邮票编号',
        'issuedate' bigint(20) NOT NULL COMMENT '邮票发布日期',
        'size' varchar(16) NOT NULL COMMENT '邮票尺寸',
-       'chikong' varchar(2) NOT NULL COMMENT '邮票齿孔',
+       'chikong' varchar(3) NOT NULL COMMENT '邮票齿孔',
        'format' varchar(8) NOT NULL COMMENT '邮票版式',
        'fanwei' varchar(16) NOT NULL COMMENT '防伪技术',
-       'design' varchar(3) NOT NULL COMMENT '邮票设计',
-       'edit' varchar(3) NOT NULL COMMENT '责任编辑',
+       'design' varchar(16) NOT NULL COMMENT '邮票设计',
+       'edit' varchar(16) NOT NULL COMMENT '责任编辑',
        'printing' varchar(16) NOT NULL COMMENT '印刷厂',
-       'bgid' varchar(3) COMMENT '大版id',
-       'bgsize' varchar(16) COMMENT '大版尺寸',
-       'bgnumber' varchar(16) COMMENT '大版枚数',
-       'slsize' varchar(16) COMMENT '小版尺寸',
-       'slnumber' varchar(16) COMMENT '小版枚数',
-       'zengsize' varchar(16) COMMENT '赠送版尺寸',
-       'zengnumber' varchar(16) COMMENT '赠送版枚数',
-       'xiaobennumber' varchar(3) COMMENT '小本票编号',
-       'xiaobenvalue' varchar(3) COMMENT '小本票面值',
-       'sltypevalue' varchar(3) COMMENT '小型张面值',
-       'sltypesize' varchar(16) COMMENT '小型张尺寸',
-       'sltypeoutsize' varchar(16) COMMENT '小型张外形尺寸',
-       'sltypeimage' varchar(256) COMMENT '小型张邮票主图',
-       'sltypechikong' varchar(3) COMMENT '小型张齿孔',
-       'sltypedraw' varchar(3) COMMENT '小型张绘画',
-       'backgroundinf' varchar(256) COMMENT '背景资料'); 
+       'backgroundinf' varchar(256) NOT NULL COMMENT '背景资料'); 
 
 --大版表
-DROP TABLE IF EXISTS t_bigformat;
-CREATE TABLE t_bigformat(
-       'bgid' varchar(3) PRIMARY KEY COMMENT '大版id',
+DROP TABLE IF EXISTS t_big_format;
+CREATE TABLE t_big_format(
+       'bgid' varchar(16) PRIMARY KEY COMMENT '大版id',
        'stid' varchar(16) COMMENT '邮票id',
        'bgsize' varchar(16) COMMENT '大版尺寸',
-       'bgnumber' varchar(16) COMMENT '大版枚数');
+       'bgnumber' smallint(16) COMMENT '大版枚数');
+
+--小版表
+DROP TABLE IF EXISTS t_small_format;
+CREATE TABLE t_small_format(
+       'slid' varchar(16) PRIMARY KEY COMMENT '小版id',
+       'stid' varchar(16) COMMENT '邮票id',
+       'slsize' varchar(16) COMMENT '小版尺寸',
+       'slnumber' smallint(16) COMMENT '小版枚数',
+
+--赠送版表
+DROP TABLE IF EXISTS t_zengsong;
+CREATE TABLE t_zengsong(
+       'zengid' varchar(16) PRIMARY KEY COMMENT '赠送版id',
+       'stid' varchar(16) COMMENT '邮票id',
+       'zeng_size' varchar(16) COMMENT '赠送版尺寸',
+       'zeng_number' smallint(16) COMMENT '赠送版枚数');
+
+--小本票表
+DROP TABLE IF EXISTS t_xiaoben;
+CREATE TABLE t_xiaoben(
+       'xiaobenid' varchar(16) PRIMARY KEY COMMENT '小本票id',
+       'stid' varchar(16) COMMENT '邮票id',
+       'number' varchar(16) COMMENT '小本票编号',
+       'face_value' varchar(16) COMMENT '小本票面值');
+
+--小型张表
+DROP TABLE IF EXISTS t_small_sheet;
+CREATE TABLE t_small_sheet(
+       'id' varchar(16) PRIMARY KEY COMMENT '小型张id',
+       'stid' varchar(16) COMMENT '邮票id',
+       'face_value' varchar(16) COMMENT '小型张面值',
+       'size' varchar(16) COMMENT '小型张尺寸',
+       'image' varchar(256) COMMENT '小型张邮票主图',
+       'chikong' varchar(16) COMMENT '小型张齿孔',
+       'draw' varchar(16) COMMENT '小型张绘画');
