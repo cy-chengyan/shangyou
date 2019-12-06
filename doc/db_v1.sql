8c3d0e28b3bb8164071778681441ca2297444fd6
Drew-Cheng <wdrewcheng@163.com>
2019-12-06 07:18:30 -0500

    在邮票信息表中新增’联合发行，雕刻，边饰设计，绘画，摄影‘字段；在小本票表中新增‘尺寸，发行量，设计，编辑，印刷厂’字段；在小型张表中新增‘设计’字段；在小版表中新增‘发行量’字段；新增数据表’小全张表，四连体小型张表，双联小型张表。

--- doc/db_v1.sql
+++ doc/db_v1.sql
@@ -37,12 +37,17 @@ CREATE TABLE t_stamp(
        `countryid` tinyint COMMENT '国家, 1-中国,',
        `number` varchar(32) NOT NULL COMMENT '邮票编号',
        `issued_date` char(10) COMMENT '邮票发布日期',
+       `joint_issue` varchar(32) COMMENT '联合发行',
        `size` varchar(32) COMMENT '邮票尺寸',
        `chikong` varchar(32) COMMENT '邮票齿孔',
        `format` varchar(64) COMMENT '邮票版式',
        `fanwei` varchar(64) COMMENT '防伪技术',
        `designer` varchar(64) COMMENT '邮票设计',
        `editor` varchar(64) COMMENT '责任编辑',
+       `carve` varchar(64) COMMENT '雕刻者',
+       `side_design` varchar(64) COMMENT '边饰设计',
+       `draw` varchar(64) COMMENT '绘画',
+       `shoot` varchar(64) COMMENT '摄影',
        `printing_house` varchar(32) COMMENT '印刷厂',
        `background` text COMMENT '背景资料'
 ) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='邮票详情表';
@@ -77,7 +82,8 @@ CREATE TABLE t_small_format(
        `slfid` char(8) PRIMARY KEY COMMENT '小版id',
        `stid` char(8) COMMENT '邮票id',
        `slsize` varchar(64) COMMENT '小版尺寸',
-       `slnumber` varchar(64) COMMENT '小版枚数'
+       `slnumber` varchar(64) COMMENT '小版枚数',
+       `issued_number` varchar(64) COMMENT '发行量'
 ) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='小版表';
 CREATE INDEX i_small_format_stid ON t_small_format(`stid`);
 
@@ -97,7 +103,12 @@ CREATE TABLE t_xiaoben(
        `xiaobenid` char(8) PRIMARY KEY COMMENT '小本票id',
        `stid` char(8) COMMENT '邮票id',
        `number` varchar(32) COMMENT '小本票编号',
-       `face_value` varchar(8) COMMENT '小本票面值'
+       `size` varchar(64) COMMENT '尺寸',
+       `issued_number` varchar(64) COMMENT '发行量',
+       `face_value` varchar(8) COMMENT '小本票面值',
+       `designer` varchar(64) COMMENT '邮票设计',
+       `editor` varchar(64) COMMENT '责任编辑',
+       `printing_house` varchar(32) COMMENT '印刷厂'
 ) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='小本票表';
 CREATE INDEX i_xiaoben_stid ON t_xiaoben(`stid`);
 
@@ -110,7 +121,36 @@ CREATE TABLE t_small_sheet(
        `size` varchar(64) COMMENT '小型张尺寸',
        `image` varchar(256) COMMENT '小型张邮票主图',
        `chikong` varchar(8) COMMENT '小型张齿孔',
+       `designer`  varchar(64) COMMENT '邮票设计',
        `draw` varchar(32) COMMENT '小型张绘画'
 ) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='小型张表';
 CREATE INDEX i_small_sheet_stid ON t_small_sheet(`stid`);
 
+--小全张
+DROP TABLE IF EXISTS t_mini_sheet;
+CREATE TABLE t_mini_sheet(
+       `minid` char(8) PRIMARY KEY COMMENT '小全张id',
+       `stid` char(8) COMMENT '邮票id',
+       `face_value` varchar(8) COMMENT '面值',
+       `size` varchar(64) COMMENT '尺寸'
+) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='小全张表';
+CREATE INDEX i_mini_sheet_stid ON t_mini_sheet(`stid`);
+
+--四连体小型张
+DROP TABLE IF EXISTS t_four_sheet;
+CREATE TABLE t_four_sheet(
+       `fsid` char(8) PRIMARY KEY COMMENT '四连体小型张id',
+       `stid` char(8) COMMENT '邮票id',
+       `face_value` varchar(8) COMMENT '面值',
+       `size` varchar(64) COMMENT '尺寸'
+) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='四连体小型张';
+CREATE INDEX i_four_sheet_stid ON t_four_sheet(`stid`);
+
+--双联小型张
+DROP TABLE IF EXISTS t_double_sheet;
+CREATE TABLE t_double_sheet(
+       `dsid` char(8) PRIMARY KEY COMMENT '双联小型张id',
+       `stid` char(8) COMMENT '邮票id',
+       `size` varchar(64) COMMENT '尺寸'
+) ENGINE='InnoDB' DEFAULT CHARACTER SET utf8mb4 COMMENT='双联小型张表';
+CREATE INDEX i_double_sheet_stid ON t_double_sheet(`stid`);
