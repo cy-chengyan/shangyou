06516435a4f418ef4e749c87f8c2e2345cbb7b10
chengyan <donald.chun@me.com>
2019-12-11 11:25:40 +0800

    add swagger & jackson json serialize.

---
+++ shangyou/core/src/main/java/shangyou/core/model/StampDetail.java
@@ -0,0 +1,11 @@
+package shangyou.core.model;
+
+import java.util.List;
+
+public class StampDetail {
+
+    private BaseStamp baseStamp;
+
+    private List<SubStamp> subStamps;
+
+}
--- shangyou/core/src/main/java/shangyou/core/model/SubStamp.java
+++ shangyou/core/src/main/java/shangyou/core/model/SubStamp.java
@@ -1,5 +1,8 @@
 package shangyou.core.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Builder
@@ -7,8 +10,10 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("子邮票")
 public class SubStamp {
 
+    @ApiModelProperty("子邮票id")
     private String sstid;
 
     private String stid;
@@ -19,8 +24,10 @@ public class SubStamp {
 
     private String picture;
 
+    @JsonProperty("face_value")
     private String faceValue;
 
+    @JsonProperty("issued_number")
     private String issuedNumber;
 
 }
