df8a05cf85fa5c73e8569baecbac8e4207850635
Drew-Cheng <wdrewcheng@163.com>
2019-12-17 16:28:02 +0800

    "生成四位随机验证码"

--- shangyou/core/src/main/java/shangyou/core/controller/CheckCodeController.java
+++ shangyou/core/src/main/java/shangyou/core/controller/CheckCodeController.java
@@ -5,14 +5,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import shangyou.core.data.redis.CheckCodeRepo;
 
+
 @Component
 public class CheckCodeController extends BaseController {
 
     @Autowired
     private CheckCodeRepo checkCodeRepo;
 
+    public static String randomCheckCode() {
+
+        String[] strs = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String code = "";
+        for (int j=0; j<4; j++){
+            int i = (int)(Math.random()*10);
+            code = code + strs[i];
+        }
+        System.out.println(code);
+        return code;
+    }
+
     private boolean sendCheckCode(String mobileNumber) {
-        String checkCode = "1234"; // TODO: 长度为4的数字验证码
+        String checkCode = randomCheckCode(); // TODO: 长度为4的数字验证码
 
 
         // TODO: 调用第三方平台的API，把短信发出去
@@ -29,3 +42,5 @@ public class CheckCodeController extends BaseController {
     }
 
 }
+
+
