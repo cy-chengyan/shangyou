cb3bd5939d4ee7d5e4e4ff5d94be32efdf2f72a9
Donald <donald.chun@me.com>
2019-12-23 10:01:29 -0500

    完成导入es数据

--- data/youpiaowu/es/es_index.py
+++ data/youpiaowu/es/es_index.py
@@ -8,8 +8,9 @@ import requests
 def add_index(line):
     data = json.loads(line)
     stid = data['stid']
-    r = requests.put('http://192.168.7.7:9200/stamp/' + stid + '/_create', json = data)
-    print(r)
+    url = 'http://192.168.7.7:9200/stamp/_doc/' + stid
+    r = requests.put(url, json = data)
+    print(url, r.json())
 
 with open('../stamp.json') as f:
     line = f.readline()
