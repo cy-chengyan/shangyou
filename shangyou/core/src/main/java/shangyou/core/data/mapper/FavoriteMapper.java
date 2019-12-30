e5f5809b469acce3edfaca279a96ed586f102014
chengyan <donald.chun@me.com>
2019-12-30 11:01:17 +0800

    添加返回收藏邮票信息的接口。

--- shangyou/core/src/main/java/shangyou/core/data/mapper/FavoriteMapper.java
+++ shangyou/core/src/main/java/shangyou/core/data/mapper/FavoriteMapper.java
@@ -2,6 +2,7 @@ package shangyou.core.data.mapper;
 
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+import shangyou.core.model.BaseStamp;
 import shangyou.core.model.Favorite;
 
 import java.util.List;
@@ -55,4 +56,21 @@ public interface FavoriteMapper {
             @Result(property = "createdAt", column = "created_at")
     })
     List<Favorite> queryFavoriteByUid(@Param("uid")String uid);
+
+    @Select({
+            "<script>",
+            "select t.stid, `type`, year, `name`, countryid, `number`, issued_date, joint_issue, size, chikong, format, fanwei, designer,",
+            " editor, carve, side_design, draw, shoot, printing_house, background, picture FROM",
+            " t_stamp as t join t_favorite as f on t.stid = f.stid WHERE uid = #{uid} AND f.status = 1",
+            " ORDER BY f.created_at DESC",
+            "<if test=\"offset >= 0 and size > 0\"> LIMIT ${offset}, #{size}</if>",
+            "</script>"
+    })
+    @Results({
+            @Result(property = "issuedDate", column = "issued_date"),
+            @Result(property = "jointIssue", column = "joint_issue"),
+            @Result(property = "sideDesign", column = "side_design"),
+            @Result(property = "printingHouse", column = "printing_house")
+    })
+    List<BaseStamp> queryFavoriteStamp(@Param("uid")String uid, int offset, int size);
 }
