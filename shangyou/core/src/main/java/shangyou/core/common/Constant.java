d507e292addeb85a1aeee9102ff06e622afddd0d
Drew-Cheng <wdrewcheng@163.com>
2020-01-03 12:42:30 +0800

    修改访问图片路径为域名地址

--- shangyou/core/src/main/java/shangyou/core/common/Constant.java
+++ shangyou/core/src/main/java/shangyou/core/common/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 
     public static final String MOBILE_NUMBER_REGEX = "^1\\d{10}$";
 //    public static final String IMG_URL_PATH_PREFIX = "https://media.shangyou.life/img/";
-    public static final String IMG_URL_PATH_PREFIX = "http://49.235.164.199/thumbnail/";
+    public static final String IMG_URL_PATH_PREFIX = "http://media.shangyou.life/thumbnail/";
 
 }
